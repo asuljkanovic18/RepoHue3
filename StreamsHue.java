7572f91049085ceb26b18108b4931014e2f855c3
Anel Suljkanovic <anelsuljkanovic04@gmail.com>
2020-10-20 11:30:26 +0200

    test

--- StreamsHue.java
+++ StreamsHue.java
@@ -6,6 +6,8 @@
 package streamshue.RepoHue3;
 
 import java.io.IOException;
+import java.util.List;
+import streamshue.RepoHue3.Weapons.Printable;
 
 /**
  *
@@ -20,8 +22,31 @@ public class StreamsHue {
         Weapons w = new Weapons("",CombatTyp.MELEE,DamageTyp.BLUNT,1,2,3,4);
         
         w.Reader("weapons.csv");
+        
+        printsorted();
+        printtable();
+        
     }
     
+    public static void printsorted(){
+        Printable print = (List<Weapons> weapon) -> {
+            for (Weapons weapons : weapon) {
+                System.out.println(weapons.sorted().toString()); 
+            }
+        };
+    }
     
+    public static void printtable(){
+        Printable print = (List<Weapons> weapon) -> {
+            for (Weapons weapons : weapon) {
+                System.out.println(weapons.getName() +"  |   "+ weapons.getCombattyp()+"  |   "+ weapons.getDamagetyp()+"  |   "+ weapons.getDamage()+"  |   "
+                        + weapons.getStrength()+"  |   "+ weapons.getSpeed()+"  |   "+ weapons.getValue()); 
+            }
+        };
+    }
+    
+    public static void s(){
+        
+    }
     
 }
--- Weapons.java
+++ Weapons.java
@@ -6,11 +6,12 @@
 package streamshue.RepoHue3;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,7 +19,7 @@ import java.util.logging.Logger;
  *
  * @author anels
  */
-public class Weapons {
+public class Weapons{
     private String name;
     private CombatTyp combattyp;
     private DamageTyp damagetyp;
@@ -38,11 +39,45 @@ public class Weapons {
         this.value = value;
     }
 
+    Weapons() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public ArrayList<Weapons> getWeapon() {
+        return weapon;
+    }
+    
+    
+
     public int getDamage() {
         return damage;
     }
-    
 
+    public String getName() {
+        return name;
+    }
+
+    public CombatTyp getCombattyp() {
+        return combattyp;
+    }
+
+    public DamageTyp getDamagetyp() {
+        return damagetyp;
+    }
+    
+    
     
     
     public void Reader(String files) throws FileNotFoundException, IOException{
@@ -60,7 +95,7 @@ public class Weapons {
               DamageTyp damagetyp = DamageTyp.valueOf(line[2].toUpperCase());
               int damage = Integer.parseInt(line[3]);
               int speed = Integer.parseInt(line[4]);
-              int stregth = Integer.parseInt(line[5]);
+              int strength = Integer.parseInt(line[5]);
               int value = Integer.parseInt(line[6]);
               weapon.add(new Weapons(name, combattyp, damagetyp, damage, speed, strength, value));
               
@@ -71,7 +106,85 @@ public class Weapons {
         br.close();
     }
     
-    public void sort(){
+    public ArrayList<Weapons> damagesort(){
        weapon.sort((d1, d2) -> d1.getDamage() - d2.getDamage());
+       return weapon;
+    }
+    
+    public ArrayList<Weapons> sorted(){
+        weapon.sort((c1, c2) -> c1.getCombattyp().compareTo(c2.getCombattyp()));
+        for (int i = 0; i < weapon.size(); i++) {
+            if(weapon.get(i).getCombattyp().equals(weapon.get(i+1).getCombattyp())){
+                weapon.sort((d1, d2) -> d1.getDamagetyp().compareTo(d2.damagetyp));
+                if(weapon.get(i).getDamagetyp().equals(weapon.get(i).getDamagetyp())){
+                    weapon.sort((n1, n2) -> n1.getName().compareTo(n2.name));
+                }
+            }
+        }
+        return weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapons{" + "name=" + name + ", combattyp=" + combattyp + ", damagetyp=" + damagetyp + ", damage=" + damage + ", speed=" + speed + ", strength=" + strength + ", value=" + value + ", weapon=" + weapon + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + Objects.hashCode(this.combattyp);
+        hash = 37 * hash + Objects.hashCode(this.damagetyp);
+        hash = 37 * hash + this.damage;
+        hash = 37 * hash + this.speed;
+        hash = 37 * hash + this.strength;
+        hash = 37 * hash + this.value;
+        hash = 37 * hash + Objects.hashCode(this.weapon);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Weapons other = (Weapons) obj;
+        if (this.damage != other.damage) {
+            return false;
+        }
+        if (this.speed != other.speed) {
+            return false;
+        }
+        if (this.strength != other.strength) {
+            return false;
+        }
+        if (this.value != other.value) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (this.combattyp != other.combattyp) {
+            return false;
+        }
+        if (this.damagetyp != other.damagetyp) {
+            return false;
+        }
+        if (!Objects.equals(this.weapon, other.weapon)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    public interface Printable{
+        void print(List<Weapons> weapons);
     }
 }
